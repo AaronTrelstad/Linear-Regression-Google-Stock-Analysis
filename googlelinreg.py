e8e0b49decd41e9f995695f800b79904a456b156
Aaron Trelstad <108637601+AaronTrelstad@users.noreply.github.com>
2022-12-06 13:22:26 -0600

    Update googlelinreg.py

--- googlelinreg.py
+++ googlelinreg.py
@@ -1,5 +1,5 @@
 from statistics import mean
-import numpy as np
+import numpy
 import csv
 
 yh = 0
@@ -17,14 +17,14 @@ with open('googlehigh.csv') as f:
 ymh = yh/(len(google))
 pmh = ph/(len(google))
 
-def slope_intercept(ymh, pmh, tyh, th):
+def slope_intercepth(ymh, pmh, tyh, th):
     mh = ((ymh*pmh) - mean(th)) / ((ymh*ymh) - mean(tyh))
 
     bh = pmh - mh*ymh
 
     return mh, bh
 
-mh, bh = slope_intercept(ymh, pmh, tyh, th)
+mh, bh = slope_intercepth(ymh, pmh, tyh, th)
 
 ##print(f"High: y = {mh}x + {bh}")
 
@@ -44,19 +44,28 @@ with open('googleaverage.csv') as f:
 yma = ya/(len(google))
 pma = pa/(len(google))
 
-def slope_intercept(yma, pma, ta, tya):
+def slope_intercepta(yma, pma, ta, tya):
     ma = ((yma*pma) - mean(ta)) / ((yma*yma) - mean(tya))
 
     ba = pma - ma*yma
 
     return ma, ba
 
-ma, ba = slope_intercept(yma, pma, ta, tya)
+ma, ba = slope_intercepta(yma, pma, ta, tya)
 
 total = 0
 for a in range(len(google)):
     total += (float(google[a][1]) - ma)**2
 sd = (total/(len(google)))**(1/2)
+
+cor = 0
+resi = 0
+tot = 0
+for c in range(len(google)):
+    resi += (float(google[c][1]) - ma)**2
+    tot += (float(google[c][1])**2)
+cor = 1 - (resi/tot)
+
 
 ##print(f"Average: y = {ma}x + {ba}")
 
@@ -76,14 +85,14 @@ with open('googlelow.csv') as f:
 yml = yl/(len(google))
 pml = pl/(len(google))
 
-def slope_intercept(yml, pml, tl, tyl):
+def slope_interceptl(yml, pml, tl, tyl):
     ml = ((yml*pml) - mean(tl)) / ((yml*yml) - mean(tyl))
 
     bl = pml - ml*yml
 
     return ml, bl
 
-ml, bl = slope_intercept(yml, pml, tl, tyl)
+ml, bl = slope_interceptl(yml, pml, tl, tyl)
 
 ##print(f"Low: y = {ml}x + {bl}")
 
@@ -103,19 +112,29 @@ with open('google5year.csv') as f:
 ym5 = y5/(len(google))
 pm5 = p5/(len(google))
 
-def slope_intercept(ym5, pm5, t5, ty5):
+def slope_intercept5a(ym5, pm5, t5, ty5):
     m5 = ((ym5*pm5) - mean(t5)) / ((ym5*ym5) - mean(ty5))
 
     b5 = pm5 - m5*ym5
 
     return m5, b5
 
-m5, b5 = slope_intercept(ym5, pm5, t5, ty5)
+m5, b5 = slope_intercept5a(ym5, pm5, t5, ty5)
+
+##print(f"5 year average: y = {m5}x + {b5}")
 
 total5 = 0
 for a in range(len(google)):
     total5 += (float(google[a][1]) - m5)**2
 sd5 = (total5/(len(google)))**(1/2)
+
+cor5 = 0
+resi5 = 0
+tot5 = 0
+for c in range(len(google)):
+    resi5 += (float(google[c][1]) - m5)**2
+    tot5 += (float(google[c][1])**2)
+cor5 = 1 - (resi5/tot5)
 
 y5l = 0
 p5l = 0
@@ -133,14 +152,14 @@ with open('google5yearlow.csv') as f:
 ym5l = y5l/(len(google))
 pm5l = p5l/(len(google))
 
-def slope_intercept(ym5l, pm5l, t5l, ty5l):
+def slope_intercept5l(ym5l, pm5l, t5l, ty5l):
     m5l = ((ym5l*pm5l) - mean(t5l)) / ((ym5l*ym5l) - mean(ty5l))
 
     b5l = pm5l - m5l*ym5l
 
     return m5l, b5l
 
-m5l, b5l = slope_intercept(ym5l, pm5l, t5l, ty5l)
+m5l, b5l = slope_intercept5l(ym5l, pm5l, t5l, ty5l)
 
 ##print(f"5 year lows: y = {m5l}x + {b5l}")
 
@@ -160,21 +179,22 @@ with open('google5yearhigh.csv') as f:
 ym5h = y5h/(len(google))
 pm5h = p5h/(len(google))
 
-def slope_intercept(ym5h, pm5h, t5h, ty5h):
+def slope_intercept5h(ym5h, pm5h, t5h, ty5h):
     m5h = ((ym5h*pm5h) - mean(t5h)) / ((ym5h*ym5h) - mean(ty5h))
 
     b5h = pm5h - m5h*ym5h
 
     return m5h, b5h
 
-m5h, b5h = slope_intercept(ym5h, pm5h, t5h, ty5h)
+m5h, b5h = slope_intercept5h(ym5h, pm5h, t5h, ty5h)
 
-
+##print(f"5 year high: y = {m5h}x + {b5h}")
 
 value = float(input("What year would you like to predict? "))
-
-print(f"Using data since 2006 would predict a price estimate of ${round((ma*value) + ba, 2)} in the year {int(value)}, but using more recent data sice 2017 the estimated price would be ${round((m5*value) + b5, 2)} which is {round(((m5*value) + b5) / ((ma*value) + ba), 2)} times greater.")
-print(f"The average return since 2006 is {round(ma, 2)}% and the average return since 2017 is {round(m5, 2)}%.")
-print(f"The 2006 data estimates a price range of ${round((ml*value) + bl, 2)} to ${round((mh*value) + bh, 2)} whereas the 2017 data predicts a range of ${round((m5l*value) + b5l, 2)} to ${round((m5h*value) + b5h, 2)} the range of the 2017 data is {round((((m5l*value) + b5l) - ((m5h*value) + b5h)) / (((ml*value) + bl) - ((mh*value) + bh)), 2)} times greater.")
-print(f"Using 2006 data the standard deviation is {round(sd,2)}, from this we would consider an unusaul high price to be ${round(((ma*value) + ba) + 2*sd, 2)} and unusaully low at ${round(((ma*value) + ba) - 2*sd, 2)}")
-print(f"Using 2017 data the standard deviation is {round(sd5,2)}, from this we would consider an unusaul high price to be ${round(((m5*value) + b5) + 2*sd5, 2)} and unusaully low at ${round(((m5*value) + b5) - 2*sd5, 2)}")
+print(f"Price Estimate:${round((ma*value) + ba, 2)}(2006-current), ${round((m5*value) + b5, 2)}(2017-current), 2017 is {round(((m5*value) + b5) / ((ma*value) + ba), 2)} times higher")
+print(f"Average Return:{round(ma, 2)}% since 2006 and {round(m5, 2)}% since 2017")
+print(f"Using 2006 data the standard deviation is {round(sd,2)}, meaning there is a 68% chance the stock price is between ${round(((ma*value) + ba) - sd, 2)} and ${round(((ma*value) + ba) + sd, 2)} and there is a 95% chance the stock price is between ${round(((ma*value) + ba) - 2*sd, 2)} and ${round(((ma*value) + ba) + 2*sd, 2)}.")
+print(f"Using 2017 data the standard deviation is {round(sd5,2)}, meaning there is a 68% chance the stock price is between ${round(((m5*value) + b5) - sd5, 2)} and ${round(((m5*value) + b5) + sd5, 2)} and there is a 95% chance the stock price is between ${round(((m5*value) + b5) - 2*sd5, 2)} and ${round(((m5*value) + b5) + 2*sd5, 2)}.")
+print(f"The r^2 value for 2006 is {round(cor, 3)} meaning that only {round(cor *100, 1)}% of price varience is caused by time.")
+print(f"The r^2 value for 2017 is {round(cor5, 3)} meaning that only {round(cor5 *100, 1)}% of price varience is caused by time.")
+print(f"This shows that the data from 2017 has a stronger linear correlation than the data from 2006, this could be caused by the 2006 data set having more outliers such as the 2008 financial crisis." )
