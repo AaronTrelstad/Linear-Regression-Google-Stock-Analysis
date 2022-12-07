66226a4603c1ac4bab9808ba50036fe19eaae937
Aaron Trelstad <108637601+AaronTrelstad@users.noreply.github.com>
2022-12-07 10:53:44 -0600

    Update googlelinreg.py

--- googlelinreg.py
+++ googlelinreg.py
@@ -8,14 +8,14 @@ th = []
 tyh = []
 with open('googlehigh.csv') as f:
     reader = csv.reader(f)
-    google = list(reader)
-    for x in range(len(google)):
-        th.append(float(google[x][1]) * float(google[x][0]))
-        tyh.append(float(google[x][0]) * float(google[x][0]))
-        yh += float(google[x][0])
-        ph += float(google[x][1])
-ymh = yh/(len(google))
-pmh = ph/(len(google))
+    googleh = list(reader)
+    for x in range(len(googleh)):
+        th.append(float(googleh[x][1]) * float(googleh[x][0]))
+        tyh.append(float(googleh[x][0]) * float(googleh[x][0]))
+        yh += float(googleh[x][0])
+        ph += float(googleh[x][1])
+ymh = yh/(len(googleh))
+pmh = ph/(len(googleh))
 
 def slope_intercepth(ymh, pmh, tyh, th):
     mh = ((ymh*pmh) - mean(th)) / ((ymh*ymh) - mean(tyh))
@@ -34,14 +34,14 @@ ta = []
 tya = []
 with open('googleaverage.csv') as f:
     reader = csv.reader(f)
-    google = list(reader)
-    for x in range(len(google)):
-        ta.append(float(google[x][1]) * float(google[x][0]))
-        tya.append(float(google[x][0]) * float(google[x][0]))
-        ya += float(google[x][0])
-        pa += float(google[x][1])
-yma = ya/(len(google))
-pma = pa/(len(google))
+    googlea = list(reader)
+    for x in range(len(googlea)):
+        ta.append(float(googlea[x][1]) * float(googlea[x][0]))
+        tya.append(float(googlea[x][0]) * float(googlea[x][0]))
+        ya += float(googlea[x][0])
+        pa += float(googlea[x][1])
+yma = ya/(len(googlea))
+pma = pa/(len(googlea))
 
 def slope_intercepta(yma, pma, ta, tya):
     ma = ((yma*pma) - mean(ta)) / ((yma*yma) - mean(tya))
@@ -51,19 +51,17 @@ def slope_intercepta(yma, pma, ta, tya):
     return ma, ba
 
 ma, ba = slope_intercepta(yma, pma, ta, tya)
-
 total = 0
-for a in range(len(google)):
-    total += (float(google[a][1]) - ma)**2
-sd = (total/(len(google)))**(1/2)
-
+for a in range(len(googlea)):
+    total += (float(googlea[a][1]) - pma)**2
+sd = (total/(len(googlea)))**(1/2)
 cor = 0
 resi = 0
 tot = 0
 c = 0
-for c in range(len(google)):
-    resi += (float(google[c][1]) - (ma * (2022 - c) + ba))**2
-    tot += (float(google[c][1]) - ma)**2
+for c in range(len(googlea)):
+    resi += (float(googlea[c][1]) - (ma * (2022 - c) + ba))**2
+    tot += (float(googlea[c][1]) - pma)**2
 cor = 1 - (resi/tot)
 
 
@@ -76,14 +74,14 @@ tyl = []
 
 with open('googlelow.csv') as f:
     reader = csv.reader(f)
-    google = list(reader)
-    for x in range(len(google)):
-        tl.append(float(google[x][1]) * float(google[x][0]))
-        tyl.append(float(google[x][0]) * float(google[x][0]))
-        yl += float(google[x][0])
-        pl += float(google[x][1])
-yml = yl/(len(google))
-pml = pl/(len(google))
+    googlel = list(reader)
+    for x in range(len(googlel)):
+        tl.append(float(googlel[x][1]) * float(googlel[x][0]))
+        tyl.append(float(googlel[x][0]) * float(googlel[x][0]))
+        yl += float(googlel[x][0])
+        pl += float(googlel[x][1])
+yml = yl/(len(googlel))
+pml = pl/(len(googlel))
 
 def slope_interceptl(yml, pml, tl, tyl):
     ml = ((yml*pml) - mean(tl)) / ((yml*yml) - mean(tyl))
@@ -103,14 +101,14 @@ ty5 = []
 
 with open('google5year.csv') as f:
     reader = csv.reader(f)
-    google = list(reader)
-    for x in range(len(google)):
-        t5.append(float(google[x][1]) * float(google[x][0]))
-        ty5.append(float(google[x][0]) * float(google[x][0]))
-        y5 += float(google[x][0])
-        p5 += float(google[x][1])
-ym5 = y5/(len(google))
-pm5 = p5/(len(google))
+    google5 = list(reader)
+    for x in range(len(google5)):
+        t5.append(float(google5[x][1]) * float(google5[x][0]))
+        ty5.append(float(google5[x][0]) * float(google5[x][0]))
+        y5 += float(google5[x][0])
+        p5 += float(google5[x][1])
+ym5 = y5/(len(google5))
+pm5 = p5/(len(google5))
 
 def slope_intercept5a(ym5, pm5, t5, ty5):
     m5 = ((ym5*pm5) - mean(t5)) / ((ym5*ym5) - mean(ty5))
@@ -122,17 +120,17 @@ def slope_intercept5a(ym5, pm5, t5, ty5):
 m5, b5 = slope_intercept5a(ym5, pm5, t5, ty5)
 
 total5 = 0
-for a in range(len(google)):
-    total5 += (float(google[a][1]) - m5)**2
-sd5 = (total5/(len(google)))**(1/2)
+for a in range(len(google5)):
+    total5 += (float(google5[a][1]) - pm5)**2
+sd5 = (total5/(len(google5)))**(1/2)
 
 cor5 = 0
 resi5 = 0
 tot5 = 0
 c5 = 0
-for c5 in range(len(google)):
-    resi5 += (float(google[c5][1]) - (m5 * (2022 - c5) + b5))**2
-    tot5 += (float(google[c5][1]) - m5)**2
+for c5 in range(len(google5)):
+    resi5 += (float(google5[c5][1]) - (m5 * (2022 - c5) + b5))**2
+    tot5 += (float(google5[c5][1]) - pm5)**2
 cor5 = 1 - (resi5/tot5)
 
 y5l = 0
@@ -142,14 +140,14 @@ ty5l = []
 
 with open('google5yearlow.csv') as f:
     reader = csv.reader(f)
-    google = list(reader)
-    for x in range(len(google)):
-        t5l.append(float(google[x][1]) * float(google[x][0]))
-        ty5l.append(float(google[x][0]) * float(google[x][0]))
-        y5l += float(google[x][0])
-        p5l += float(google[x][1])
-ym5l = y5l/(len(google))
-pm5l = p5l/(len(google))
+    google5l = list(reader)
+    for x in range(len(google5l)):
+        t5l.append(float(google5l[x][1]) * float(google5l[x][0]))
+        ty5l.append(float(google5l[x][0]) * float(google5l[x][0]))
+        y5l += float(google5l[x][0])
+        p5l += float(google5l[x][1])
+ym5l = y5l/(len(google5l))
+pm5l = p5l/(len(google5l))
 
 def slope_intercept5l(ym5l, pm5l, t5l, ty5l):
     m5l = ((ym5l*pm5l) - mean(t5l)) / ((ym5l*ym5l) - mean(ty5l))
@@ -169,14 +167,14 @@ ty5h = []
 
 with open('google5yearhigh.csv') as f:
     reader = csv.reader(f)
-    google = list(reader)
-    for x in range(len(google)):
-        t5h.append(float(google[x][1]) * float(google[x][0]))
-        ty5h.append(float(google[x][0]) * float(google[x][0]))
-        y5h += float(google[x][0])
-        p5h += float(google[x][1])
-ym5h = y5h/(len(google))
-pm5h = p5h/(len(google))
+    google5h = list(reader)
+    for x in range(len(google5h)):
+        t5h.append(float(google5h[x][1]) * float(google5h[x][0]))
+        ty5h.append(float(google5h[x][0]) * float(google5h[x][0]))
+        y5h += float(google5h[x][0])
+        p5h += float(google5h[x][1])
+ym5h = y5h/(len(google5h))
+pm5h = p5h/(len(google5h))
 
 def slope_intercept5h(ym5h, pm5h, t5h, ty5h):
     m5h = ((ym5h*pm5h) - mean(t5h)) / ((ym5h*ym5h) - mean(ty5h))
